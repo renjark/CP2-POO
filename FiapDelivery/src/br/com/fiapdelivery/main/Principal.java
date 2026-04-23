188b07113d297748bb9b564ce74f40435fc2fa61
Krajner <laiskrajnerlacerda@gmail.com>
2026-04-23 10:36:17 -0300

    Atualização de código

--- FiapDelivery/src/br/com/fiapdelivery/main/Principal.java
+++ FiapDelivery/src/br/com/fiapdelivery/main/Principal.java
@@ -1,10 +1,3 @@
-package br.com.fiapdelivery.main;
-
-import br.com.fiapdelivery.model.Caminhao;
-import br.com.fiapdelivery.model.Moto;
-import br.com.fiapdelivery.model.Pacote;
-import br.com.fiapdelivery.model.Rota;
-
 public class Principal {
 
     public static void main(String[] args) {
@@ -12,14 +5,15 @@ public class Principal {
         System.out.println("=== FIAP DELIVERY ===");
 
         Caminhao caminhao = new Caminhao("NEO9738", 1000, 6);
-        Pacote pacote = new Pacote("BR873", 10.5);
-
-        Rota rota1 = new Rota(pacote, caminhao);
-        rota1.iniciarEntrega();
-
         Moto moto = new Moto("LOP3619", 50, true);
 
-        Rota rota2 = new Rota(pacote, moto);
-        rota2.iniciarEntrega();
+        Pacote pacote1 = new Pacote("BR873", 10.5);
+        Pacote pacote2 = new Pacote("BR999", 200);
+
+        Rota rota1 = new Rota(pacote1, caminhao);
+        rota1.iniciarEntrega();
+
+        Rota rota2 = new Rota(pacote2, moto);
+        rota2.iniciarEntrega(); 
     }
 }
